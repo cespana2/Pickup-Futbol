a74eb43862e88989f8d937094436e918aaeecc30
Carlos Espana Jr <cespana2@horizon.csueastbay.edu>
2024-05-25 00:35:42 -0700

    Added a .env file

--- src/config/Firebase.js
+++ src/config/Firebase.js
@@ -1,14 +1,14 @@
 import firebase from "firebase";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyAtj6621uv5LbOzYPnC7ozX1FbK6QtrEwM",
-  authDomain: "pickup-futbol.firebaseapp.com",
-  databaseURL: "https://pickup-futbol-default-rtdb.firebaseio.com",
-  projectId: "pickup-futbol",
-  storageBucket: "pickup-futbol.appspot.com",
-  messagingSenderId: "950393325921",
-  appId: "1:950393325921:web:5bb039ed92a52eaefe5d20",
-  measurementId: "G-N6263PM8FZ"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
+  appId: process.env.REACT_APP_FIREBASE_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
